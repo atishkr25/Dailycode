f94b90355d420737a24346c1a8f4b692aa19ecbd
atishkumar <atishkumar6115@gmail.com>
2024-12-27 01:00:14 +0530

    count set bit(num of 1's) in binary

--- DSA/BIT SET/new01.cpp
+++ DSA/BIT SET/new01.cpp
@@ -1,20 +1,14 @@
+// counts numbers of 1's( set bit ) in binary
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif
- 
  int n; cin>>n;
  int cnt=0;
  while(n!=0){
   if(n&1)
     cnt++;
-  
   n=n>>1;
  }
 cout<<cnt<<endl;
