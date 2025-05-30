28600210778b57f510c07f659ab25ca8f3271633
atishkumar <atishkumar6115@gmail.com>
2025-05-31 04:05:52 +0530

    new file added

--- DSA/Recursion/Prnt-subsqnce-rec.cpp
+++ DSA/Recursion/Prnt-subsqnce-rec.cpp
@@ -1,15 +1,44 @@
-//printing subsequences using recursion 
 #include<bits/stdc++.h>
 using namespace std;
+ 
+#define nl endl
+#define ll long long int
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb push_back
+#define f first
+#define s second
+#define MOD 1000000007
+ 
+#define vi vector<int>
+#define vll vector<ll>
+ 
+#define fr(i, a, b) for (ll i = a; i < b; i++)
 
+void PrntSubsequense(int i, vi &ds, vi &a, int n) {
+    if(i >= n){
+        for(auto x : ds){
+            cout<<x<<" ";
+        }
+        cout<<nl;
+        return;
+    }
+    ds.push_back(a[i]);
+    PrntSubsequense(i+1, ds, a, n);
+    ds.pop_back();
+    PrntSubsequense(i+1, ds, a, n);
+}
+void sol()
+{
+    vi a = {3,1,2};
+    int n = a.size();
+    vi ds;
+    PrntSubsequense(0, ds, a, n);
 
+}
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif
-
+ int t = 1;
+ while(t--)
+ sol(); 
 }
