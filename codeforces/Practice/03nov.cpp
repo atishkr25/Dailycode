e2016b75f70d3b406c41d4e95a148d61090dc517
git config user.namegit config user.name <atishkumar6115@gmail.com>
2026-01-09 19:23:12 +0530

    comming on track....

---
+++ codeforces/Practice/03nov.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io cin.sync_with_stdio(false); cin.tie(0); cout.tie(0)
+// #define cerr if(false)cerr
+#define int long long
+#define ll long long
+#define pb push_back
+#define eb emplace_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(x) (int)(x).size()
+#define MOD 1000000007
+#define INF (int)1e16
+#define endl "\n"
+#define input(a, n) vi a(n); for(int i = 0; i < n; i++) cin >> a[i];
+#define yes cout << "YES" << endl; return;
+#define no cout << "NO" << endl; return;
+
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define ai(zzz) array<int, zzz>
+#define pii pair<int, int>
+
+// #define f(i, a, b) for (int i = a; i < b; i++)
+// #define fr(i, a, b) for (int i = a; i > b; i--)
+
+void sol() {
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    if(a == b && b == c && c == d) {
+        yes
+    }
+    else no
+}
+
+int32_t main() {
+    fast_io;
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        sol();
+    }
+    
+}
