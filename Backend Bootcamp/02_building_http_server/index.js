4302382bda2c1b48e782d3ce965fdc3e8bdf3bf1
git config user.namegit config user.name <atishkumar6115@gmail.com>
2026-01-11 02:21:46 +0530

    using nodemon(to avoiding ctrl+c close and restart server

---
+++ Backend Bootcamp/02_building_http_server/index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req , res)=>{
+
+    const log = `${Date.now()} : ${req.url} : New Request Recieved \n`;
+    fs.appendFile('log.txt' , log , (err , data)=>{
+
+        switch(req.url){
+            case '/' : res.end("Home ");
+            break;
+
+            case '/about' : res.end("hey, atish this side");
+            break;
+
+            default:
+                res.end("404 Not Found");
+
+        }
+        
+        
+    })
+
+
+    // console.log(req.headers)
+    // console.log(req)
+    
+
+});
+
+const PORT = 8000;
+myServer.listen(8000, ()=>{
+    console.log(`Server started at port no ${PORT} `);
+})
